dc7c560f29ac255d42f6657f80a634fe3a90e1d6
reda frauly <reda.frauly@gmail.com>
2020-04-06 00:55:40 +0200

    WRITE sourcemap only in dev mode

--- fragment-node/rollup.config.js
+++ fragment-node/rollup.config.js
@@ -9,7 +9,7 @@ const production = !process.env.ROLLUP_WATCH;
 export default {
 	input: 'src/main.js',
 	output: {
-		sourcemap: true,
+		sourcemap: !production,
 		format: 'iife',
 		name: 'fragment', // == manifest.json.name
 		file: 'public/bundle.js' // manifest.json.js
@@ -21,7 +21,7 @@ export default {
 			// we'll extract any component CSS out into
 			// a separate file — better for performance
 			css: css => {
-				css.write('public/bundle.css');
+				css.write('public/bundle.css', !production);
 			}
 		}),
 
