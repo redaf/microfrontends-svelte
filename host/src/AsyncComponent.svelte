d5a6e55e72c28dff39ea36d4f95460c4687cfb75
reda frauly <reda.frauly@gmail.com>
2020-04-28 17:52:35 +0200

    ADD css support to AsyncComponent

--- host/src/AsyncComponent.svelte
+++ host/src/AsyncComponent.svelte
@@ -3,7 +3,13 @@
   export let prefix;
 
   let componentLoader = loadManifest().then(manifest => {
-    const { name, js } = manifest;
+    const { name, js, css } = manifest;
+
+    const style = document.createElement("link");
+    style.rel = "stylesheet";
+    style.href = `${prefix}${css}`;
+    document.head.appendChild(style);
+
     const script = document.createElement("script");
     script.src = `${prefix}${js}`;
     script.type = "text/javascript";
@@ -13,6 +19,7 @@
       };
     });
     document.body.appendChild(script);
+
     return component;
   });
 </script>
